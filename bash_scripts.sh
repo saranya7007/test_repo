2a25ffb5d014b228bfcc1ed34bcc2d7fd8ad0da0
Saranya Sampath <saranya180698@gmail.com>
2024-02-04 19:38:02 +0530

    first commit

---
+++ bash_scripts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+my_name="Saranya"
+my_job="Design engineer"
+
+echo "My name is $my_name.Working as $my_job."
+
+echo $(ls)
+echo $(pwd)
+
+echo $(date)
+
+echo " User name is $USER"
